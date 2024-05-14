a54de2c50c70cfa932913704ce8c7a4d900c5082
Andrei <example@gmail.com>
2024-05-14 14:05:21 -0230

    if

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,72 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
+// ПРАКТИКА Ч1. НАЧИНАЕМ СОЗДАВАТЬ ПРИЛОЖЕНИЕ.
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('Как вы его оцениваете?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('Как вы его оцениваете?', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('Как вы его оцениваете?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('Как вы его оцениваете?', '');
 
-console.log(personalMovieDB);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// УСЛОВИЯ
+
+// Если 4 равняется 4 тогда сообщение 'Ok!' должно отобразиться в консоли.
+// if (4 == 4) {
+//     console.log('Ok!');
+// }
+
+// Если 4 не равняется 4 тогда сообщение 'Error!' должно отобразиться в консоли.
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!');
+// }
+
+
+// Если переменная num меньше 49 - это ошибка, 
+// если больше - это много, если равняется - это ок!
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+
+// Тернарный оператор
+
+// Если условие верно, тогда ок! если нет, тогда error!
+// (num === 50) ? console.log('Ok!'): console.log('Error!');
+
+// switch - модификация конструкции if
+// const num = 51;
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно!');
+//         break;
+//     case 100:
+//         console.log('Неверно!');
+//     case 50:
+//         console.log('В точку!');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
